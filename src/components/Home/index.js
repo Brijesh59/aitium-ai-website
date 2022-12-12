732e245954e9dd0877ad1a8f4067ee817ebfc4b1
Brijesh <kumarbrijesh9021@gmail.com>
2022-12-12 13:53:14 +0530

    Clypeus => Aitium

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,33 +1,41 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
-import Modal from 'react-modal';
-import { NavBar, Button, Heading, HeroContainer, ImageContainer, Paragraph, AppContainer } from '../common'
+import Modal from "react-modal";
+import {
+  NavBar,
+  Button,
+  Heading,
+  HeroContainer,
+  ImageContainer,
+  Paragraph,
+  AppContainer,
+} from "../common";
 
 import backgroundRightSvg from "../../assets/background-right.svg";
 import Contact from "../Contact";
 import JoinUs from "../JoinUs";
 
 const customStyles = {
-  content : {
-    top                   : '40%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
+  content: {
+    top: "40%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
     minWidth: 300,
     padding: "10px",
     background: "#F3F4F3",
     borderRadius: 2,
-  }
+  },
 };
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 export default function App() {
   return (
     <AppContainer>
-      <div style={{width: '86%', marginLeft: '7%'}}>
-        <NavBar>CLYPEUS.AI</NavBar>
+      <div style={{ width: "86%", marginLeft: "7%" }}>
+        <NavBar>Aitium.AI</NavBar>
         <Body />
       </div>
     </AppContainer>
@@ -41,13 +49,13 @@ function Body() {
   function openModal() {
     setIsOpen(true);
   }
-  function closeModal(){
+  function closeModal() {
     setIsOpen(false);
   }
   function openModal2() {
     setIsOpen2(true);
   }
-  function closeModal2(){
+  function closeModal2() {
     setIsOpen2(false);
   }
   return (
@@ -60,59 +68,84 @@ function Body() {
             AND ANALYSIS <br />
           </Heading>
           <Paragraph>
-            We are a stealth mode AI Startup, empowering real time Insights and Threat intelligence. Get AI powered security alerts on your fingertips.
+            We are a stealth mode AI Startup, empowering real time Insights and
+            Threat intelligence. Get AI powered security alerts on your
+            fingertips.
           </Paragraph>
-          <Button onClick={openModal}>
-            CONTACT US
-          </Button>
+          <Button onClick={openModal}>CONTACT US</Button>
           <Button outlined onClick={openModal2}>
             JOIN MAIL LIST
           </Button>
         </HeroContainer>
         <ImageContainer>
-          <img 
-            src={backgroundRightSvg} 
-            className={classes.img} 
+          <img
+            src={backgroundRightSvg}
+            className={classes.img}
             draggable="false"
-            alt="background-right" />
+            alt="background-right"
+          />
         </ImageContainer>
       </div>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
-        overlayClassName={classes.overlayStyles} >
-        <img src={require('../../assets/close-icon.svg')} style={{float: 'right', marginRight: '5%', cursor:'pointer', width: 30, height:30}} onClick={closeModal} alt="icon-cross" />
+        overlayClassName={classes.overlayStyles}
+      >
+        <img
+          src={require("../../assets/close-icon.svg")}
+          style={{
+            float: "right",
+            marginRight: "5%",
+            cursor: "pointer",
+            width: 30,
+            height: 30,
+          }}
+          onClick={closeModal}
+          alt="icon-cross"
+        />
         <Contact closeModal={closeModal} />
       </Modal>
       <Modal
-       isOpen={modalIsOpen2}
-       onRequestClose={closeModal2}
-       style={customStyles}
-       overlayClassName={classes.overlayStyles} >
-        <img src={require('../../assets/close-icon.svg')} style={{float: 'right', marginRight: '5%', cursor:'pointer', width: 30, height:30}} onClick={closeModal2}  alt="icon-cross" />
-      <JoinUs closeModal={closeModal2} />
-    </Modal>
+        isOpen={modalIsOpen2}
+        onRequestClose={closeModal2}
+        style={customStyles}
+        overlayClassName={classes.overlayStyles}
+      >
+        <img
+          src={require("../../assets/close-icon.svg")}
+          style={{
+            float: "right",
+            marginRight: "5%",
+            cursor: "pointer",
+            width: 30,
+            height: 30,
+          }}
+          onClick={closeModal2}
+          alt="icon-cross"
+        />
+        <JoinUs closeModal={closeModal2} />
+      </Modal>
     </>
   );
 }
 
 const useStyles = createUseStyles({
-  overlayStyles:{
-    position: 'fixed',
+  overlayStyles: {
+    position: "fixed",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
-    background: 'rgba(0,0,0,0.6)',
+    background: "rgba(0,0,0,0.6)",
   },
-  body:{
-    display: 'flex',
-    flexWrap: 'wrap'
+  body: {
+    display: "flex",
+    flexWrap: "wrap",
   },
   img: {
-    width: '100%',
-    marginTop: '-100px',
-    userSelect: 'none'
-  }
+    width: "100%",
+    marginTop: "-100px",
+    userSelect: "none",
+  },
 });
