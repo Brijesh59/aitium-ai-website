bc0dfd81777ba0e287a5ddf8957a5954d6c865d2
Brijesh <kumarbrijesh9021@gmail.com>
2022-12-13 20:58:11 +0530

    add emailjs client to send form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,124 +1,185 @@
-import React from "react";
-import { Button, Input, TextArea } from '../common'
-import axios from 'axios'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default function({closeModal}) {
-  const [loading, setLoading] = React.useState(false)
-  const [errorName, setErrorName] = React.useState('')
-  const [errorEmail, setErrorEmail] = React.useState('')
-  const [errorMessage, setErrorMessage] = React.useState('')
-  const [name, setName] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [message, setMessage] = React.useState('')
+import { Button, Input, TextArea } from "../common";
+
+export default function ({ closeModal }) {
+  const [loading, setLoading] = React.useState(false);
+  const [errorName, setErrorName] = React.useState("");
+  const [errorEmail, setErrorEmail] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const form = useRef();
 
   const validate = () => {
-    setErrorName('')
-    setErrorEmail('')
-    setErrorMessage('')
-    const emailValidate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
-    if(!name)
-      return {
-        isValid: false,
-        errorName: 'name',
-        error: "Name is required"
-      }
+    setErrorName("");
+    setErrorEmail("");
+    setErrorMessage("");
+    const emailValidate =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if(!email)
+    if (!name)
       return {
         isValid: false,
-        errorName: 'email',
-        error: "Email is required"
-      }
-    if(!emailValidate.test(email)){
+        errorName: "name",
+        error: "Name is required",
+      };
+
+    if (!email)
       return {
         isValid: false,
-        errorName: 'email',
-        error: "Invalid Email"
-      }
-    } 
-    if(!message)
+        errorName: "email",
+        error: "Email is required",
+      };
+    if (!emailValidate.test(email)) {
       return {
         isValid: false,
-        errorName: 'message',
-        error: "Message is required"
-      }
+        errorName: "email",
+        error: "Invalid Email",
+      };
+    }
+    if (!message)
+      return {
+        isValid: false,
+        errorName: "message",
+        error: "Message is required",
+      };
     return {
       isValid: true,
-      error: ""
-    }
-  }
+      error: "",
+    };
+  };
 
-  const handleChange=(e)=>{
-    switch(e.target.name){
-      case 'name': setName(e.target.value); break;
-      case 'email': setEmail(e.target.value); break;
-      case 'message': setMessage(e.target.value); break;
-      default: return;
-     }
-  }
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case "name":
+        setName(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "message":
+        setMessage(e.target.value);
+        break;
+      default:
+        return;
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const {isValid, error, errorName} = validate()
-    if(!isValid) {
-      switch(errorName){
-        case 'name': return setErrorName(error);
-        case 'email': return setErrorEmail(error);
-        case 'message': return setErrorMessage(error);
-        default: return 
+    const { isValid, error, errorName } = validate();
+    if (!isValid) {
+      switch (errorName) {
+        case "name":
+          return setErrorName(error);
+        case "email":
+          return setErrorEmail(error);
+        case "message":
+          return setErrorMessage(error);
+        default:
+          return;
       }
     }
 
-    setErrorName('')
-    setErrorEmail('')
-    setErrorMessage('')
-    setLoading(true)
-    
-    axios.post(
-      'https://clypeus-send-mail.herokuapp.com/clypeusmail',
-      {senderName: name, senderEmail: email, message},
-      {headers:{'Content-Type': 'application/json'}}
-    )
-    .then(data => {
-      setLoading(false)
-      closeModal()
-      alert("We have recieved your details. We Will reply soon.")
-    })
-    .catch(error=>{
-      setLoading(false)
-      closeModal()
-      alert(error.message)
-    })
+    setErrorName("");
+    setErrorEmail("");
+    setErrorMessage("");
+    setLoading(true);
 
-  }
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_CONTACT_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setLoading(false);
+          closeModal();
+          alert("We have recieved your message. We will get back to you soon.");
+        },
+        (error) => {
+          setLoading(false);
+          closeModal();
+          alert(error.message);
+        }
+      );
+  };
   return (
-    <div style={{width: '90%', margin: '0px auto', textAlign: 'center', display: 'flex', flexDirection: 'column'}}>
-      <h2 style={{color: '#2A2B2A',fontWeight: 700}}>GET IN TOUCH</h2>
-      <Input type="text" placeholder="Your name" name="name" value={name} onChange={handleChange} disabled={loading}/> 
-      {
-        errorName && <pre style={{marginTop: -5, textAlign: 'left', color: 'red'}}>{errorName}</pre>
-      }  
-      <Input type="email" placeholder="Your Email" name="email" value={email} onChange={handleChange} disabled={loading}/>
-      {
-        errorEmail && <pre style={{marginTop: -5, textAlign: 'left', color: 'red'}}>{errorEmail}</pre>
-      } 
-      <TextArea type="text" placeholder="Your Message" name="message" value={message} onChange={handleChange} disabled={loading} rows="3"/> 
-      {
-        errorMessage && <pre style={{marginTop: -5, textAlign: 'left', color: 'red'}}>{errorMessage}</pre>
-      }   
-      <Button type="submit" onClick={handleSubmit} style={btnStyle} disabled={loading}>
-            {loading ? 'SENDING ...' : 'SEND MESSAGE'} 
-      </Button>
+    <div
+      style={{
+        width: "90%",
+        margin: "0px auto",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h2 style={{ color: "#2A2B2A", fontWeight: 700 }}>GET IN TOUCH</h2>
+
+      <form onSubmit={handleSubmit} ref={form}>
+        <Input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          disabled={loading}
+        />
+        {errorName && (
+          <pre style={{ marginTop: -5, textAlign: "left", color: "red" }}>
+            {errorName}
+          </pre>
+        )}
+        <Input
+          type="email"
+          placeholder="Your Email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          disabled={loading}
+        />
+        {errorEmail && (
+          <pre style={{ marginTop: -5, textAlign: "left", color: "red" }}>
+            {errorEmail}
+          </pre>
+        )}
+        <TextArea
+          type="text"
+          placeholder="Your Message"
+          name="message"
+          value={message}
+          onChange={handleChange}
+          disabled={loading}
+          rows="3"
+        />
+        {errorMessage && (
+          <pre style={{ marginTop: -5, textAlign: "left", color: "red" }}>
+            {errorMessage}
+          </pre>
+        )}
+        <Button
+          type="submit"
+          onClick={handleSubmit}
+          style={btnStyle}
+          disabled={loading}
+        >
+          {loading ? "SENDING ..." : "SEND MESSAGE"}
+        </Button>
+      </form>
     </div>
   );
 }
 
 const btnStyle = {
-  borderRadius: 5, 
+  borderRadius: 5,
   padding: 8,
-  margin: 0, 
+  margin: 0,
   marginBottom: 10,
   marginTop: 10,
-}
+};
--- src/components/JoinUs/index.js
+++ src/components/JoinUs/index.js
@@ -1,74 +1,108 @@
-import React from "react";
-import axios from 'axios';
-import { Button, Input } from '../common'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
-export default function({closeModal}) {
-  const [loading, setLoading] = React.useState(false)
-  const [error, setError] = React.useState('')
-  const [email, setEmail] = React.useState('')
+import { Button, Input } from "../common";
+
+export default function ({ closeModal }) {
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [email, setEmail] = React.useState("");
+
+  const form = useRef();
 
   const validate = () => {
-    const emailValidate = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
- 
-    if(!email)
+    const emailValidate =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!email)
       return {
         isValid: false,
-        error: "Email is required"
-      }
-    if(!emailValidate.test(email)){
+        error: "Email is required",
+      };
+    if (!emailValidate.test(email)) {
       return {
         isValid: false,
-        error: "Invalid Email"
-      }
+        error: "Invalid Email",
+      };
     }
     return {
       isValid: true,
-      error: ""
-    }
-  }
+      error: "",
+    };
+  };
 
-  const handleChange=(e)=>{
-    setEmail(e.target.value)
-  }
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const {isValid, error} = validate()
-    if(!isValid) return setError(error)
+    const { isValid, error } = validate();
+    if (!isValid) return setError(error);
 
-    setError('')
-    setLoading(true)
+    setError("");
+    setLoading(true);
 
-    axios.post(
-      'https://clypeus-send-mail.herokuapp.com/clypeusmail',
-      {senderEmail: email},
-      {headers:{'Content-Type': 'application/json'}}
-    )
-    .then(data => {
-      setLoading(false)
-      closeModal()
-      alert("Your email has been submitted successfully.")
-    })
-    .catch(error=>{
-      setLoading(false)
-      closeModal()
-      alert(error.message)
-    })
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_NEWSLETTER_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setLoading(false);
+          closeModal();
+          alert("Thank you for subscribing!");
+        },
+        (error) => {
+          setLoading(false);
+          closeModal();
+          alert(error.message);
+        }
+      );
+
     // closeModal()
-  }
+  };
   return (
-    <div style={{width: '90%', margin: '0px auto', textAlign: 'center', display: 'flex', flexDirection: 'column'}}>
-      <h2 style={{color: '#2A2B2A',fontWeight: 700}}>JOIN MAIL LIST</h2>
-      <div style={{color: '#606464',fontWeight: 400, marginTop: -10}}>
+    <div
+      style={{
+        width: "90%",
+        margin: "0px auto",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h2 style={{ color: "#2A2B2A", fontWeight: 700 }}>JOIN MAIL LIST</h2>
+      <div style={{ color: "#606464", fontWeight: 400, marginTop: -10 }}>
         Join our product mail list to get latest updates.
-      </div> <br />
-      <form onSubmit={handleSubmit}>
-        <Input type="email" placeholder="Your Email" required value={email} onChange={handleChange} name="email" disabled={loading}/>
-        {
-          error && <pre style={{marginTop: -5, textAlign: 'left', color: 'red'}}>{error}</pre>
-        }      
-        <Button type="submit" onClick={handleSubmit} style={btnStyle} disabled={loading}>
-          {loading ? 'SENDING ...' : 'SEND EMAIL'} 
+      </div>{" "}
+      <br />
+      <form onSubmit={handleSubmit} ref={form}>
+        <Input
+          type="email"
+          placeholder="Your Email"
+          required
+          value={email}
+          onChange={handleChange}
+          name="email"
+          disabled={loading}
+        />
+        {error && (
+          <pre style={{ marginTop: -5, textAlign: "left", color: "red" }}>
+            {error}
+          </pre>
+        )}
+        <Button
+          type="submit"
+          onClick={handleSubmit}
+          style={btnStyle}
+          disabled={loading}
+        >
+          {loading ? "SENDING ..." : "SEND EMAIL"}
         </Button>
       </form>
     </div>
@@ -76,10 +110,10 @@ export default function({closeModal}) {
 }
 
 const btnStyle = {
-  borderRadius: 5, 
+  borderRadius: 5,
   padding: 8,
-  margin: 0, 
+  margin: 0,
   marginBottom: 10,
   marginTop: 10,
-  width: "100%"
-}
+  width: "100%",
+};
